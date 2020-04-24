2404e7e34cacce0385fe99c865aa1fa98cff6f94
saiprakashmarella <saiprakashmarella3277@gmail.com>
2020-04-24 13:48:02 +0530

    Added unit testing for rest API using MockMVC

--- AllTest/src/test/java/com/example/demo/AllTestApplicationTests.java
+++ AllTest/src/test/java/com/example/demo/AllTestApplicationTests.java
@@ -7,34 +7,43 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.example.demo.controller.TestController;
 import com.example.demo.dao.TestDao;
 import com.example.demo.model.data;
 import com.example.demo.repository.TestRepository;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class AllTestApplicationTests {
+	
+	public MockMvc mockMvc;
 	
 	@Autowired
 	@InjectMocks
 	TestDao td;
 	
+
 	@MockBean
 	TestRepository tr;
 	
-	
+	@InjectMocks
+	public TestController testController;
+		
 	
 	@Test
 	void showDataCheck() {
@@ -47,6 +56,14 @@ class AllTestApplicationTests {
 		data d=new data("Sai",25,"India");
 		when(tr.save(d)).thenReturn(d);
 		assertEquals(d, td.savedata(d));
+	}
+	
+	@Test
+	void showDataUrlCheck() throws Exception{
+		mockMvc=MockMvcBuilders.standaloneSetup(testController).build();
+		mockMvc.perform(MockMvcRequestBuilders.get("/test"))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.content().string("Hello World"));
 	}
 	
 
